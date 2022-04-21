8a0a375e1d5c9b8b212dfb4652b52f43e57c45bb
fushuwei <331044761@qq.com>
2022-04-22 01:34:19 +0800

    创建项目

---
+++ smartdi-core/src/main/java/com/mochousoft/smartdi/core/Engine.java
@@ -0,0 +1,15 @@
+package com.mochousoft.smartdi.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 入口类
+ */
+public class Engine {
+
+    private static final Logger logger = LoggerFactory.getLogger(Engine.class);
+
+    public static void main(String[] args) {
+    }
+}
